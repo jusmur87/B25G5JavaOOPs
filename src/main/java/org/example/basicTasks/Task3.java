bce14b988700629adbeb8b66a97b959d9b32b89c
jusmur87 <justyna.murdza1234@gmail.com>
2026-01-10 20:22:49 -0500

    task 12345

--- src/main/java/org/example/basicTasks/Task3.java
+++ src/main/java/org/example/basicTasks/Task3.java
@@ -3,4 +3,14 @@ package org.example.basicTasks;
 /*Reverse a String: Write a function to reverse a given string. For example, given the
   input "Hello", the output should be "olleH".*/
 public class Task3 {
+    public static void main(String[] args) {
+        String word = "desserts"; // Even with a capital 'D'
+
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        System.out.println("Reading forward: " + word);
+        System.out.println("Reading backward: " + reversed);
+
+    }
 }
+
